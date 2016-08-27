10aecdd85c7b82693a7f27fc31061b9dc0438d3d
ophid1an <dermentzoglou.ioannis@gmail.com>
2016-08-27 19:14:12 +0300

    fourth commit

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
 var express = require('express');
+var multer = require('multer');
+var md5File = require('md5-file');
+var fs = require('fs');
+
+var upload = multer({dest: './uploads/'});
+
 var app = express();
 
 app.set('port', (process.env.PORT || 8000));
 
 app.use(express.static(__dirname + '/public'));
 
-app.listen(app.get('port'), function() {
+app.post('/results', upload.single('fileToUpload'), function (req, res, next) {
+  var arr = [req.body, req.file];
+  arr.forEach(function (val) {
+    console.log(val);
+  });
+  md5File(req.file.path, function (err, hash) {
+    if (err) throw err;
+    res.send({
+      size: req.file.size,
+      md5sum: hash
+    });
+    fs.unlink(req.file.path, function (err) {
+      if (err) throw err;
+    });
+  });
+
+});
+
+app.listen(app.get('port'), function () {
   console.log('Node app is running on port', app.get('port'));
 });
